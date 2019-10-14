49a5a68fbdebc606662974a5aaf033a1b25178aa
vanessadcamara <vanessadcamara@gmail.com>
2019-10-14 19:03:26 -0400

    Adicionando opções do jogo

--- modules/menu.py
+++ modules/menu.py
@@ -30,4 +30,27 @@ if __name__ == "__main__":
     title.write("ATARI COMBAT TANK", font=style, align="center")
     title.hideturtle()
 
+    # desenhando opções
+
+    play = turtle
+    play.setposition(0, 50)
+    play.color("yellow")
+    style = ("Fixedsys", 20, "bold")
+    play.write("play game", font=style, align="center")
+    play.hideturtle()
+
+    credit = turtle
+    credit.setposition(0, 0)
+    credit.color("yellow")
+    style = ("Fixedsys", 20, "bold")
+    credit.write("credits", font=style, align="center")
+    credit.hideturtle()
+
+    exit_game = turtle
+    exit_game.setposition(0, -50)
+    exit_game.color("yellow")
+    style = ("Fixedsys", 20, "bold")
+    exit_game.write("exit", font=style, align="center")
+    exit_game.hideturtle()
+    
     screen.mainloop()
