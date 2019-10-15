38d54771a7600b6fb4d85f24c88f3daea611629e
Vinicius-Soares <vsdc.snf19@uea.edu.br>
2019-10-14 21:18:58 -0400

    mapeamento de movimentação

--- modules/level.py
+++ modules/level.py
@@ -1,10 +1,10 @@
 import turtle as tt
 
-def generate(level):
+def generate(file):
     # manipulação do arquivo
     #file = open('/home/williams_gomes/vscode/github/atari-combat-tank/level1.txt')
-    file = open(level)
-    lvl = file.readlines()
+    level = open(file)
+    lvl = level.readlines()
     for i in range(len(lvl)):
         lvl[i] = lvl[i].strip('\n')
     
@@ -14,7 +14,7 @@ def generate(level):
 
     for i in range(29):
         for j in range(36):
-            if lvl[i][j] != '0':
+            if int(lvl[i][j]):
                     block = tt.Turtle()
                     block.shape("square")
                     block.color("white")
@@ -24,5 +24,27 @@ def generate(level):
             x += 20
         y -= 20
         x = -350
-        
-    file.close()
+
+    level.close()
+
+
+def nav_generate(file):
+    # manipulação do arquivo
+    level = open(file)
+    nav = level.readlines()
+    for i in range(len(nav)):
+        nav[i] = nav[i].strip('\n')
+
+    # x e y baseado nas dimensões da tela
+    x = -350
+    y = 280
+    navigation_mesh = []
+
+    for i in range(58):
+        navigation_mesh.append([0] * 72)
+        for j in range(72):
+            navigation_mesh[i][j] = int(nav[i][j])
+            x += 10
+        y -= 10
+        x = -350
+    return navigation_mesh
