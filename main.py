7e0a7e1bb0613689210e550d4a9cc210e5f5ebf0
Vinicius-Soares <vsdc.snf19@uea.edu.br>
2019-10-23 14:48:20 -0400

    pontuacao

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ import modules.menu as menu
 
 screen = tt.Screen()
 command_history = set()
+hud1 = None
+hud2 = None
+score_1 = 0
+score_2 = 0
 
 
 def create_game():
@@ -17,6 +21,24 @@ def create_game():
     screen.setup(720, 580)  # 720,480 antigo
     screen.tracer(0)
     screen.mode("logo")
+
+    hud1 = tt.Turtle()
+    hud1.speed(0)
+    hud1.shape("square")
+    hud1.color("red")
+    hud1.penup()
+    hud1.hideturtle()
+    hud1.goto(-250, 200)
+    hud1.write("0", align="center", font=("Press Start 2P",48,"normal") )
+
+    hud2 = tt.Turtle()
+    hud2.speed(0)
+    hud2.shape("square")
+    hud2.color("blue")
+    hud2.penup()
+    hud2.hideturtle()
+    hud2.goto(250, 200)
+    hud2.write("0", align="center", font=("Press Start 2P",48,"normal") )
 
     tank.register_tank_shape(screen)
     tank.first_player = tank.create_player("red", -280, -50)
@@ -41,6 +63,8 @@ def create_game():
     screen.onkeypress(start_second_player_rotate_right, "Right")
     screen.onkeyrelease(stop_second_player_rotate_right, "Right")
     screen.onkeypress(fire.second_player_fire, "Down")
+
+    return hud1, hud2
 
 
 # Comandos primeiro jogador
@@ -100,11 +124,15 @@ def stop_second_player_rotate_right():
 
 
 def run():
+    global hud1
+    global hud2
+    global score_1
+    global score_2
     if len(menu.jogar) > 0 and menu.jogar[0] == "creating_menu":
         menu.create_menu(screen)
         menu.jogar[0] = "waiting"
     if len(menu.jogar) > 0 and menu.jogar[0] == "starting":
-        create_game()
+        hud1, hud2 = create_game()
         menu.jogar[0] = "playing"
     if len(menu.jogar) > 0 and menu.jogar[0] == "playing":
         fire.move_ball(fire.first_player_ball,
@@ -113,8 +141,14 @@ def run():
                        tank.second_player, tank.first_player)
         if tank.first_player.is_respawning:
             tank.die(tank.first_player)
+            score_2 += 1
+            hud2.clear()
+            hud2.write("{}".format(score_2), align="center", font=("Press Start 2P",48,"normal") )
         if tank.second_player.is_respawning:
             tank.die(tank.second_player)
+            score_1 += 1
+            hud1.clear()
+            hud1.write("{}".format(score_1), align="center", font=("Press Start 2P",48,"normal") )
         for command in command_history:
             command()
     screen.update()
